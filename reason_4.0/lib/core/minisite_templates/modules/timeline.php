c156cdb63a2493208b6775804bea60ee8e99bf4d
Quinn Shanahan <q.shanahan@gmail.com>
2016-03-23 22:25:07 -0400

    build json

--- reason_4.0/lib/core/minisite_templates/modules/timeline.php
+++ reason_4.0/lib/core/minisite_templates/modules/timeline.php
@@ -3,9 +3,16 @@
 	//reason_include_once( 'classes/timeliner.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'TimelineModule';
-	
+
 	class TimelineModule extends DefaultMinisiteModule
 	{
+		function init( $args = array() )
+		{
+			$head_items = $this->get_head_items();
+			$head_items->add_javascript('https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js');
+			$head_items->add_stylesheet('https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css');
+			$head_items->add_stylesheet('https://cdn.knightlab.com/libs/timeline3/latest/css/fonts/font.abril-droidsans.css');
+		}
 
 		function has_content()
 		{
@@ -14,7 +21,7 @@
 			$es->add_type( id_of('timeline_type'));
 			$es->add_right_relationship($this->cur_page->id(), relationship_id_of('page_to_timeline'));
 			$es->add_rel_sort_field($this->cur_page->id(), relationship_id_of('page_to_timeline'));
-			$es->set_order('rel_sort_order'); 
+			$es->set_order('rel_sort_order');
 			$posts = $es->run_one();
 			if (count($posts) > 0)
 			{
@@ -22,7 +29,7 @@
 			}
 			return false;
 		}
-		
+
 		function run()
 		{
 			$site_id = $this->site_id;
@@ -30,21 +37,37 @@
 			$es->add_type( id_of('timeline_type'));
 			$es->add_right_relationship($this->cur_page->id(), relationship_id_of('page_to_timeline'));
 			$es->add_rel_sort_field($this->cur_page->id(), relationship_id_of('page_to_timeline'));
-			$es->set_order('rel_sort_order'); 
+			$es->set_order('rel_sort_order');
 			$timelines = $es->run_one();
-			
+
 			foreach($timelines as $timeline)
 			{
+				$timeline_dom_id = uniqid('timeline_');
+				$json = [
+					'title' => [],
+					'events' => [],
+					'eras' => [],
+					'scale' => 'human'
+				];
+
 				$es = new entity_selector($this->site_id);
 				$es->add_type(id_of('timeline_item_type'));
 				$es->add_right_relationship($timeline->_id, relationship_id_of('timeline_to_timeline_item'));
 				$timeline_items = $es->run_one();
-				
+
 				foreach($timeline_items as $timeline_item)
 				{
-					echo '<pre>';
-					print_r($timeline_item);
-					echo '</pre><br>';
+					$timeline_item_json = [
+						'start_date' => [
+							'year'  => intval(date('Y', strtotime($timeline_item->get_value('start_date')))),
+							'month' => intval(date('m', strtotime($timeline_item->get_value('start_date')))),
+							'day'   => intval(date('d', strtotime($timeline_item->get_value('start_date'))))
+						],
+						'text' => [
+							'text' => $timeline_item->get_value('text')
+						]
+					];
+
 					if ($timeline_item->get_value('media') == 'reason_image')
 					{
 						$es = new entity_selector($this->site_id);
@@ -52,12 +75,19 @@
 						$es->add_right_relationship($timeline_item->_id, relationship_id_of('timeline_item_to_image'));
 						$es->set_num(1);
 						$images = $es->run_one();
+
 						if (!empty($images))
 						{
 							$image = reset($images);
-							print_r($image);
-							echo '<img src="' . WEB_PHOTOSTOCK . $image->_id . '.' . $image->get_value('image_type') . '"/></a>'."<br>\n";
-							echo $image->get_value('description')."<br><br>\n";
+							// print_r($image);
+							// echo '<img src="' . WEB_PHOTOSTOCK . $image->_id . '.' . $image->get_value('image_type') . '"/></a>'."<br>\n";
+							// echo $image->get_value('description')."<br><br>\n";
+
+							$timeline_item_json['media'] = [
+								'url'     => WEB_PHOTOSTOCK . $image->_id . '.' . $image->get_value('image_type'),
+								'caption' => $image->get_value('description')
+							];
+							// $json['events'][] = $timeline_item_json;
 						}
 					}
 					else if ($timeline_item->get_value('media') == 'reason_media_work')
@@ -66,13 +96,15 @@
 						$es->add_type(id_of('av'));
 						$es->add_right_relationship($timeline_item->_id, relationship_id_of('timeline_item_to_media_work'));
 						$es->set_num(1);
-						$media_works = $es->run_one();
-						print_r(reset($media_works));
-						echo ' ***** end media_work *****<br><br>';
 
 						if (!empty($media_works))
 						{
+							$media_works = $es->run_one();
 							$media_work = reset($media_works);
+							var_dump($media_work);
+							echo ' ***** end media_work *****<br><br>';
+
+
 							$es = new entity_selector($this->site_id);
 							$es->add_type(id_of('av_file'));
 							$es->add_right_relationship($media_work->id(), relationship_id_of('av_to_av_file'));
@@ -80,19 +112,31 @@
 							$es->set_num(1);
 							$media_files = $es->run_one();
 							if (!empty($media_files))
-							{	
+							{
 								$media_file = reset($media_files);
 								print_r($media_file);
 								echo ' ***** end media_file *****<br><br>';
-							}	
+							}
 						}
 					}
 					else if ($timeline_item->get_value('media') == 'other')
 					{
-						echo '<b>Other media: </b>' . $timeline_item->get_value('other_media') . '<br><br>'."\n";
+						$timeline_item_json['media'] = [
+							'url' => $timeline_item->get_value('other_media')
+						];
 					}
+
+					$json['events'][] = $timeline_item_json;
 				}
+
+				$json = json_encode($json);
+
+				echo "
+				<div id=\"$timeline_dom_id\"></div>
+				<script>
+					if (!window.timelines) window.timelines = []
+					timelines.push(new TL.Timeline('$timeline_dom_id', $json))
+				</script>";
 			}
 		}
 	}
-?>
